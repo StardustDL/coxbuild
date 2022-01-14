a29f7a64a25b39fd8bca165674f9dab64b4714a7
StardustDL <stardustdl@163.com>
2022-01-14 16:53:41 +0800

    support whereis

--- src/main/coxbuild/extensions/shell/__init__.py
+++ src/main/coxbuild/extensions/shell/__init__.py
@@ -1,6 +1,26 @@
+import platform
 from coxbuild.schema import group, run
 
 task = group("shell")
+
+
+def whereCommand(name: str) -> list[str]:
+    if "windows" in platform.system().lower():
+        res = run(["where.exe", name], pipe=True, fail=True)
+        if res:
+            return [f.strip() for f in res.stdout.splitlines() if f.strip()]
+        else:
+            return []
+    else:
+        res = run(["whereis", name], pipe=True, fail=True)
+        if res:
+            return [f.strip() for f in res.stdout.split(":")[1].strip().split(" ") if f.strip()]
+        else:
+            return []
+
+
+def existCommand(name: str) -> bool:
+    return len(whereCommand(name)) > 0
 
 
 @task
