d0c44c38524812ea58255a11bdf892cce60181b0
StardustDL <stardustdl@163.com>
2022-01-14 10:54:58 +0800

    fix test script

--- test/demo/coxbuild.py
+++ test/demo/coxbuild.py
@@ -1,4 +1,4 @@
-from coxbuild.schema import precond, task, depend, run, before, after, args, kwds
+from coxbuild.schema import precond, task, depend
 
 
 @task()
@@ -14,34 +14,8 @@ def a():
 
 @depend(pre)
 @task()
-def b(*args, **kwds):
+def b():
     print("b")
-    print(args)
-    print(kwds)
-
-
-@before(b)
-def beforeB(*args, **kwds):
-    print("before b")
-    print(args)
-    print(kwds)
-
-
-@after(b)
-def afterB(*args, **kwds):
-    print("after b")
-    print(args)
-    print(kwds)
-
-
-@args(b)
-def argsB():
-    return [1, 2, 3]
-
-
-@kwds(b)
-def kwdsB():
-    return {"x": 1, "y": 2}
 
 
 @precond(lambda: False)
