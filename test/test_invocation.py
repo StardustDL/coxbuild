ab1e70d1cc0332f504ade4c4510cd334e8c4007b
StardustDL <stardustdl@163.com>
2022-01-25 09:30:12 +0800

    fix unittest bug by breaking change in invocation

--- test/test_invocation.py
+++ test/test_invocation.py
@@ -5,18 +5,18 @@ from coxbuild.invocation import CommandExecutionArgs, run
 
 
 def test_run():
-    result = run(CommandExecutionArgs(
-        ["echo", "abc"], shell=True, cwd=get_working_directory(), pipe=True))
+    result = run(
+        ["echo", "abc"], shell=True, cwd=get_working_directory(), pipe=True)
     assert result
 
 
 def test_fail():
-    result = run(CommandExecutionArgs(
-        ["cat", "abc.txt"], shell=True, cwd=get_working_directory(), pipe=True), fail=True, retry=1)
+    result = run(
+        ["cat", "abc.txt"], shell=True, cwd=get_working_directory(), pipe=True, fail=True, retry=1)
 
 
 def test_timeout():
-    result = run(CommandExecutionArgs(
-        ["ping", "bing.com"], cwd=get_working_directory(), timeout=timedelta(seconds=0.2), pipe=True), fail=True)
+    result = run(
+        ["ping", "bing.com"], cwd=get_working_directory(), timeout=timedelta(seconds=0.2), pipe=True, fail=True)
     assert not result
     assert result.timeout
